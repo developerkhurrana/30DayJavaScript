4cd8889c78d66b782419d75b55c91d640e342a6f
developerkhurrana <160478054+developerkhurrana@users.noreply.github.com>
2024-03-01 00:15:16 +0530

    day 04

--- 04/Day04.js
+++ 04/Day04.js
@@ -74,82 +74,45 @@ yearInput = 2025;
 
 month = monthInput.toUpperCase();
 
-if (yearInput % 4 == 0) {
-  switch (month) {
-    case "JANUARY":
-      console.log(month + " has 31 days");
-      break;
-    case "FEBRUARY":
-      console.log(month + " has 29 days");
-      break;
-    case "MARCH":
-      console.log(month + " has 31 days");
-      break;
-    case "APRIL":
-      console.log(month + " has 30 days");
-      break;
-    case "MAY":
-      console.log(month + " has 31 days");
-      break;
-    case "JUNE":
-      console.log(month + " has 30 days");
-      break;
-    case "JULY":
-      console.log(month + " has 31 days");
-      break;
-    case "AUGUST":
-      console.log(month + " has 31 days");
-      break;
-    case "SEPTEMBER":
-      console.log(month + " has 30 days");
-      break;
-    case "OCTOBER":
-      console.log(month + " has 31 days");
-      break;
-    case "NOVEMBER":
-      console.log(month + " has 30 days");
-      break;
-    case "DECEMBER":
-      console.log(month + " has 31 days");
-      break;
-  }
-} else {
-  switch (month) {
-    case "JANUARY":
-      console.log(month + " has 31 days");
-      break;
-    case "FEBRUARY":
-      console.log(month + " has 28 days");
-      break;
-    case "MARCH":
-      console.log(month + " has 31 days");
-      break;
-    case "APRIL":
-      console.log(month + " has 30 days");
-      break;
-    case "MAY":
-      console.log(month + " has 31 days");
-      break;
-    case "JUNE":
-      console.log(month + " has 30 days");
-      break;
-    case "JULY":
-      console.log(month + " has 31 days");
-      break;
-    case "AUGUST":
-      console.log(month + " has 31 days");
-      break;
-    case "SEPTEMBER":
-      console.log(month + " has 30 days");
-      break;
-    case "OCTOBER":
-      console.log(month + " has 31 days");
-      break;
-    case "NOVEMBER":
-      console.log(month + " has 30 days");
-      break;
-    case "DECEMBER":
-      console.log(month + " has 31 days");
-      break;
-  }
+switch (month) {
+  case "JANUARY":
+    console.log(month + " has 31 days");
+    break;
+  case "FEBRUARY":
+    {
+      yearInput % 4 == 0
+        ? console.log(month + " has 29 days")
+        : console.log(month + " has 28 days");
+    }
+    break;
+  case "MARCH":
+    console.log(month + " has 31 days");
+    break;
+  case "APRIL":
+    console.log(month + " has 30 days");
+    break;
+  case "MAY":
+    console.log(month + " has 31 days");
+    break;
+  case "JUNE":
+    console.log(month + " has 30 days");
+    break;
+  case "JULY":
+    console.log(month + " has 31 days");
+    break;
+  case "AUGUST":
+    console.log(month + " has 31 days");
+    break;
+  case "SEPTEMBER":
+    console.log(month + " has 30 days");
+    break;
+  case "OCTOBER":
+    console.log(month + " has 31 days");
+    break;
+  case "NOVEMBER":
+    console.log(month + " has 30 days");
+    break;
+  case "DECEMBER":
+    console.log(month + " has 31 days");
+    break;
 }
